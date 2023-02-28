d10cc1d7dae107a2bb0ab12e67baa89f47634a00
Nash <me@aunyks.com>
2023-02-28 11:40:46 -0500

    Soft sun. Comes with tradeoffs though

--- dist/js/graphics/prefabs/sun.module.js
+++ dist/js/graphics/prefabs/sun.module.js
@@ -1,6 +1,7 @@
 import {
   CircleGeometry,
-  MeshLambertMaterial,
+  ShaderMaterial,
+  Uniform,
   Object3D,
   Color,
   Mesh,
@@ -11,14 +12,30 @@ class Sun extends Group {
   constructor(_color, _scaleFactor) {
     super()
     const scaleFactor = _scaleFactor || 1
-    const color = (_color || new Color(0xffffff)).convertLinearToSRGB()
+    const color = (_color || new Color(0xeeeeee)).convertLinearToSRGB()
     this._sunMesh = new Mesh(
-      new CircleGeometry(5, 26),
-      new MeshLambertMaterial({
-        color: color,
-        emissive: color,
-        emissiveIntensity: 1000,
-        flatShading: true,
+      new CircleGeometry(200, 26),
+      new ShaderMaterial({
+        transparent: true,
+        vertexShader: `
+          varying vec2 vUv;
+          uniform vec3 color;
+
+          void main() {
+            vUv = uv;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+          }`,
+        fragmentShader: `
+          varying vec2 vUv;
+          uniform vec3 color;
+
+          void main() {
+            gl_FragColor = vec4(color, 1.0 - smoothstep(0.0, .5, distance(vUv, vec2(0.5))));
+          }
+          `,
+        uniforms: {
+          color: new Uniform(color),
+        },
       })
     )
     this.pivot = new Object3D()
@@ -26,13 +43,12 @@ class Sun extends Group {
     this.pivot.add(this._sunMesh)
     this.add(this.pivot)
 
-    this._sunMesh.position.set(0, 0, -40)
+    this._sunMesh.position.set(0, 0, -700)
     this._sunMesh.lookAt(0, 0, 0)
     this._sunMesh.scale.set(scaleFactor, scaleFactor, scaleFactor)
 
     // - 1 so that it's in front of the skydome
     this._sunMesh.renderOrder = -(Number.MAX_SAFE_INTEGER - 1)
-    this._sunMesh.material.depthTest = false
   }
 }
 
