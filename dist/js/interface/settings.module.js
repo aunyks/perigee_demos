15d33095d5d5ed3772e1265224a6d7ca11db1fff
Nash <me@aunyks.com>
2023-03-15 20:16:40 -0400

    Prep settings for reuse among levels

---
+++ dist/js/interface/settings.module.js
@@ -0,0 +1,89 @@
+import GUI from '/js/debug/lil-gui.module.js'
+import Stats from '/js/debug/stats.module.js'
+import { isInDebugMode } from '/js/misc/utils.module.js'
+
+function bindSettings({ sim }, setupDebugGui) {
+  const perfStatistics = new Stats()
+  document.body.appendChild(perfStatistics.dom)
+
+  const masterVolumeSlider = document.getElementById('master-volume-slider')
+  const horizSensSlider = document.getElementById('horiz-sens-slider')
+  const vertSensSlider = document.getElementById('vert-sens-slider')
+  const perfStatsCheckbox = document.getElementById('perf-stats-toggle')
+  const debugToolsCheckbox = document.getElementById('debug-tools-toggle')
+
+  if (!isInDebugMode()) {
+    for (const debugSettingLabel of Array.from(
+      document.querySelectorAll('label[for][data-debug]')
+    )) {
+      const debugInputId = debugSettingLabel.htmlFor
+      if (!debugInputId) {
+        continue
+      }
+      const debugInput = document.getElementById(debugInputId)
+      debugInput.style.display = 'none'
+      debugSettingLabel.style.display = 'none'
+    }
+  }
+
+  const settings = {
+    sim: {
+      leftRightLookSensitivity: parseInt(horizSensSlider.value),
+      upDownLookSensitivity: parseInt(vertSensSlider.value),
+    },
+    interface: {
+      masterVolume: parseFloat(masterVolumeSlider.value) / 100,
+      perfStatisticsEnabled: perfStatsCheckbox.checked,
+      debugToolsEnabled: debugToolsCheckbox.checked,
+    },
+  }
+  sim.setLeftRightLookSensitivity(settings.sim.leftRightLookSensitivity)
+  sim.setUpDownLookSensitivity(settings.sim.upDownLookSensitivity)
+  if (settings.interface.perfStatisticsEnabled) {
+    perfStatistics.showPanel()
+  }
+
+  masterVolumeSlider.addEventListener('change', (e) => {
+    settings.interface.masterVolume = parseFloat(e.target.value) / 100
+  })
+
+  horizSensSlider.addEventListener('change', (e) => {
+    sim.setLeftRightLookSensitivity(parseInt(e.target.value))
+  })
+
+  vertSensSlider.addEventListener('change', (e) => {
+    sim.setUpDownLookSensitivity(parseInt(e.target.value))
+  })
+
+  perfStatsCheckbox.addEventListener('change', (e) => {
+    const checked = e.currentTarget.checked
+    settings.interface.perfStatisticsEnabled = checked
+
+    if (checked) {
+      perfStatistics.showPanel()
+    } else {
+      perfStatistics.hideAllPanels()
+    }
+  })
+
+  let debugGui = null
+  debugToolsCheckbox.addEventListener('change', (e) => {
+    const checked = e.currentTarget.checked
+    settings.interface.debugToolsEnabled = checked
+
+    if (checked) {
+      if (debugGui === null) {
+        debugGui = new GUI()
+        setupDebugGui(debugGui)
+      } else {
+        debugGui.show()
+      }
+    } else {
+      debugGui.hide()
+    }
+  })
+
+  return perfStatistics
+}
+
+export { bindSettings }
--- dist/js/levels/0/index.js
+++ dist/js/levels/0/index.js
@@ -48,9 +48,8 @@ import {
 } from '/js/graphics/three-ext/utils.module.js'
 import SkyDome from '/js/graphics/prefabs/skydome.module.js'
 import Sun from '/js/graphics/prefabs/sun.module.js'
-import Stats from '/js/debug/stats.module.js'
-import GUI from '/js/debug/lil-gui.module.js'
 import { toggleModal, modalWithId } from '/js/components/modal.module.js'
+import { bindSettings } from '/js/interface/settings.module.js'
 
 const loadingContainer = document.getElementById('loading-container')
 const sceneContainer = document.getElementById('scene-container')
@@ -92,85 +91,8 @@ Promise.all(assetsToLoad)
       loadingContainer.remove()
       sceneContainer.classList.remove('hidden')
 
-      const perfStatistics = new Stats()
-      document.body.appendChild(perfStatistics.dom)
-
-      // Settings values
-      const masterVolumeSlider = document.getElementById('master-volume-slider')
-      const horizSensSlider = document.getElementById('horiz-sens-slider')
-      const vertSensSlider = document.getElementById('vert-sens-slider')
-      const perfStatsCheckbox = document.getElementById('perf-stats-toggle')
-      const debugToolsCheckbox = document.getElementById('debug-tools-toggle')
-
-      if (!isInDebugMode()) {
-        for (const debugSettingLabel of Array.from(
-          document.querySelectorAll('label[for][data-debug]')
-        )) {
-          const debugInputId = debugSettingLabel.htmlFor
-          if (!debugInputId) {
-            continue
-          }
-          const debugInput = document.getElementById(debugInputId)
-          debugInput.style.display = 'none'
-          debugSettingLabel.style.display = 'none'
-        }
-      }
-
-      const settings = {
-        sim: {
-          leftRightLookSensitivity: parseInt(horizSensSlider.value),
-          upDownLookSensitivity: parseInt(vertSensSlider.value),
-        },
-        interface: {
-          masterVolume: parseFloat(masterVolumeSlider.value) / 100,
-          perfStatisticsEnabled: perfStatsCheckbox.checked,
-          debugToolsEnabled: debugToolsCheckbox.checked,
-        },
-      }
-      sim.setLeftRightLookSensitivity(settings.sim.leftRightLookSensitivity)
-      sim.setUpDownLookSensitivity(settings.sim.upDownLookSensitivity)
-      if (settings.interface.perfStatisticsEnabled) {
-        perfStatistics.showPanel()
-      }
-
-      masterVolumeSlider.addEventListener('change', (e) => {
-        settings.interface.masterVolume = parseFloat(e.target.value) / 100
-      })
-
-      horizSensSlider.addEventListener('change', (e) => {
-        sim.setLeftRightLookSensitivity(parseInt(e.target.value))
-      })
-
-      vertSensSlider.addEventListener('change', (e) => {
-        sim.setUpDownLookSensitivity(parseInt(e.target.value))
-      })
-
-      perfStatsCheckbox.addEventListener('change', (e) => {
-        const checked = e.currentTarget.checked
-        settings.interface.perfStatisticsEnabled = checked
-
-        if (checked) {
-          perfStatistics.showPanel()
-        } else {
-          perfStatistics.hideAllPanels()
-        }
-      })
-
-      let debugGui = null
-      debugToolsCheckbox.addEventListener('change', (e) => {
-        const checked = e.currentTarget.checked
-        settings.interface.debugToolsEnabled = checked
-
-        if (checked) {
-          if (debugGui === null) {
-            debugGui = new GUI()
-            debugGui.add(document, 'title')
-          } else {
-            debugGui.show()
-          }
-        } else {
-          debugGui.hide()
-        }
+      const perfStatistics = bindSettings({ sim }, (debugGui) => {
+        debugGui.add(document, 'title')
       })
 
       const renderer = new WebGLRenderer({
@@ -206,7 +128,9 @@ Promise.all(assetsToLoad)
         new CapsuleGeometry(playerRadius, playerHeight - playerRadius * 2),
         new MeshBasicMaterial({ color: 0xffff00, wireframe: true })
       )
-      // mainScene.add(playerMesh)
+      if (isInDebugMode()) {
+        mainScene.add(playerMesh)
+      }
 
       sceneGltf.scene.traverse((obj) => {
         if (obj.isMesh && !obj.userData.simSettings.graphics.enabled) {
